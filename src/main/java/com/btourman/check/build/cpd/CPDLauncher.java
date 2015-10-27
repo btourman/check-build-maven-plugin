f551f6b6994df5567ff14cc8102bf292ae568f43
btourman <tourman.benjamin@gmail.com>
2015-10-27 21:43:41 +0100

    Update CPDLauncher to display report in file
    CPD display report with println, so, to check file after
    the analyze of CPD, we must store report in file

--- src/main/java/com/btourman/check/build/cpd/CPDLauncher.java
+++ src/main/java/com/btourman/check/build/cpd/CPDLauncher.java
@@ -1,6 +1,10 @@
 package com.btourman.check.build.cpd;
 
 import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import net.sourceforge.pmd.cpd.CPD;
 
@@ -11,22 +15,34 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class CPDLauncher implements ILauncher {
 
-	@Override
-	public int launch(String basedir, JsonNode confNode) {
+    @Override
+    public int launch(String basedir, JsonNode confNode) {
 
-		try {
-			CPDConf conf = confNode != null ? new ObjectMapper().readValue(confNode.toString(), CPDConf.class) : new CPDConf();
-			CPD.main(new String[] { "--files", basedir, "--minimum-tokens", String.valueOf(conf.getMinimumToken()), "--format", conf.getFormat() });
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+        try {
+            final CPDConf conf = confNode != null ? new ObjectMapper().readValue(confNode.toString(), CPDConf.class) : new CPDConf();
+            System.setOut(new PrintStream(System.out) {
+                @Override
+                public void println(String x) {
+                    Path file = Paths.get("checkbuild/cpd." + conf.getExtensionFile());
+                    try {
+                        Files.write(file, x.getBytes());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            CPD.main(new String[]{"--files", basedir, "--minimum-tokens", String.valueOf(conf.getMinimumToken()),
+                    "--format", conf.getFormat()});
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-		return 0;
-	}
+        return 0;
+    }
 
-	@Override
-	public String getName() {
-		return ModuleNameConst.CPD;
-	}
+    @Override
+    public String getName() {
+        return ModuleNameConst.CPD;
+    }
 
 }
