b4f2fffd1dd8a73b1d04dfa2d74ea2b292803c1f
Tourman <tourman.benjamin@gmail.com>
2018-04-07 11:48:38 +0200

    Update App.java
    Add semi-colon

--- src/main/java/com/btourman/check/build/App.java
+++ src/main/java/com/btourman/check/build/App.java
@@ -5,7 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Iterator;
-import java.util.ServiceLoader
+import java.util.ServiceLoader;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
